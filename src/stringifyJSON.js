42256a78585cced6c4f4fcf40f2f401695a60807
nhasty <nyall.hasty@gmail.com>
2022-09-06 15:44:31 -0700

    refactor stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // generate empty string
+  // debugger
+  var stringify = '';
+  // edge cases: function, undefined, null
+  if (typeof obj === 'function' || typeof obj === 'undefined' || obj === null) {
+    // add null
+    stringify += 'null';
+  // if primitive add to string
+  } else if (typeof obj !== 'object') {
+    if (typeof obj === 'string') {
+      stringify += '"' + obj + '"';
+    } else {
+      stringify += obj;
+    }
+  // if array add brackets
+  } else if (Array.isArray(obj)) {
+    stringify += '[';
+    obj.forEach(function(item) {
+      stringify += stringifyJSON(item) + ',';
+    });
+    if (stringify.length > 1) {
+      stringify = stringify.slice(0, -1) + ']';
+    } else {
+      stringify += ']';
+    }
+  // if object add curly brace
+  } else {
+    stringify += '{';
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+        stringify += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    if (stringify.length > 1) {
+      stringify = stringify.slice(0, -1) + '}';
+    } else {
+      stringify += '}';
+    }
+  }
+  return stringify;
 };
